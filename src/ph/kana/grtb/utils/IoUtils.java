42fcc6641b6eb75ec379643fdd60f191deb987d1
kana0011 <marklynoel.alvarez@synacy.com>
2015-10-27 19:51:57 +0800

    Implement properties file as cache

--- src/ph/kana/grtb/utils/IoUtils.java
+++ src/ph/kana/grtb/utils/IoUtils.java
@@ -3,15 +3,17 @@ package ph.kana.grtb.utils;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.OutputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Scanner;
+import java.util.Properties;
 import javax.swing.JTextArea;
 import ph.kana.grtb.process.GrailsProcess;
 
@@ -19,7 +21,13 @@ public class IoUtils {
 	
 	private IoUtils() { }
 	
-	public static final File CACHE = new File(".grtb-cache");
+	private static final File CACHE_FILE = new File(".grtb-cache");
+	private static final Properties CACHE = new Properties();
+	static {
+		loadCache();
+	}
+	
+	private static final String PREVIOUS_PROJECT = "previous_project";
 	
 	public static void printToFile(String content, File file) throws IOException {
 		try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
@@ -39,27 +47,17 @@ public class IoUtils {
 	}
 	
 	public static void saveCurrentProject(File directory) throws IOException {
-		if (directory != null) {
-			try (PrintWriter writer = new PrintWriter(CACHE)) {
-				writer.println(directory.getAbsolutePath());
-				writer.flush();
-			}
-		}
+		CACHE.setProperty(PREVIOUS_PROJECT, directory.getAbsolutePath());
+		saveCache();
 	}
 	
 	public static File fetchPreviousProject() throws IOException {
-		if (CACHE.exists()) {
-			Scanner scanner = new Scanner(CACHE);
-			
-			if (scanner.hasNextLine()) {
-				File previousProject = new File(scanner.nextLine());
-
-				if (previousProject.exists() && previousProject.isDirectory()) {
-					return previousProject;
-				}
-			}
+		String previousProjectLocation = CACHE.getProperty(PREVIOUS_PROJECT);
+		if (null == previousProjectLocation || previousProjectLocation.isEmpty()) {
+			return null;
+		} else {
+			return new File(previousProjectLocation);
 		}
-		return null;
 	}
 	
 	public static void logRunning(GrailsProcess process) {
@@ -76,6 +74,28 @@ public class IoUtils {
 	
 	public static void logProjectChange(File project) {
 		printCommandLog('>', "Grails project path set!", project.getAbsolutePath());
+	}
+	
+	private static void loadCache() {
+		try {
+			if (CACHE_FILE.exists()) {
+				InputStream cacheFileInputStream = new FileInputStream(CACHE_FILE);
+				CACHE.load(cacheFileInputStream);
+			}
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
+	}
+	
+	private static void saveCache() {
+		try {
+			OutputStream cacheFileOutputStream = new FileOutputStream(CACHE_FILE);
+			
+			String cacheHeader = "Cache for grails-toolbox.\nCan be deleted.";
+			CACHE.store(cacheFileOutputStream, cacheHeader);
+		} catch (IOException e) {
+			e.printStackTrace(System.err);
+		}
 	}
 	
 	private static void printCommandLog(char icon, String message, String command) {
