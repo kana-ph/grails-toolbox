429a6af17cbcabe177139e38404179a72d6af06c
kana0011 <kana.incognito@gmail.com>
2016-02-08 23:23:16 +0800

    Tabify
    Signed-off-by: kana0011 <kana.incognito@gmail.com>

--- src/ph/kana/grtb/ui/GrailsToolboxFrame.java
+++ src/ph/kana/grtb/ui/GrailsToolboxFrame.java
@@ -19,10 +19,10 @@ import ph.kana.grtb.utils.ComponentUtils;
 import ph.kana.grtb.utils.IoUtils;
 
 public class GrailsToolboxFrame extends javax.swing.JFrame {
-	
+
 	public GrailsToolboxFrame() {
 		initComponents();
-		
+
 		consoleTextArea.requestFocus();
 		ComponentUtils.implementAutoScroll(consoleTextArea);
 	}
@@ -83,11 +83,16 @@ public class GrailsToolboxFrame extends javax.swing.JFrame {
         setTitle("Grails Toolbox");
         setMinimumSize(new java.awt.Dimension(830, 750));
         addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
             public void windowActivated(java.awt.event.WindowEvent evt) {
                 formWindowActivated(evt);
             }
-            public void windowOpened(java.awt.event.WindowEvent evt) {
-                formWindowOpened(evt);
+        });
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                formKeyTyped(evt);
             }
         });
 
@@ -504,32 +509,32 @@ public class GrailsToolboxFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-	
+
 	private File grailsProjectDirectory;
 	private GrailsProcess currentProcess;
-	
+
 	private void enableComponents(boolean enabled) {
 		ComponentUtils.enableContainer(toolboxPanel, enabled);
 		grailsMenu.setEnabled(enabled);
-		
+
 		killProcessButton.setEnabled(!enabled);
 		progressBar.setIndeterminate(!enabled);
 	}
-	
+
 	private void executeGrailsProcess(GrailsProcess grailsProcess) throws InterruptedException, IOException {
 		currentProcess = grailsProcess;
 		currentProcess.execute();
 		IoUtils.reflectStreamToTextArea(currentProcess.getInputStream(), consoleTextArea);
 	}
-	
+
 	private void executeGrailsProcessAsBackground(final GrailsProcess grailsProcess) {
 		grailsProcess.setGrailsProjectDirectrory(grailsProjectDirectory);
 		grailsProcess.setStacktraceMode(useStacktraceCheckbox.isSelected());
-		
+
 		consoleTextArea.setText("");
 		enableComponents(false);
 		progressBar.setString(grailsProcess.getCommandString());
-		
+
 		SwingWorker worker = new SwingWorker<Void, Void>() {
 			@Override
 			protected Void doInBackground() throws Exception {
@@ -546,7 +551,7 @@ public class GrailsToolboxFrame extends javax.swing.JFrame {
 			public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
 				if ("DONE".equals(propertyChangeEvent.getNewValue().toString())) {
 					IoUtils.logExit(currentProcess);
-					
+
 					enableComponents(true);
 					progressBar.setString("");
 					currentProcess = null;
@@ -555,7 +560,7 @@ public class GrailsToolboxFrame extends javax.swing.JFrame {
 		});
 		worker.execute();
 	}
-	
+
 	private void initializeGrailsProjectDirectory() {
 		try {
 			File newDirectory = fetchGrailsDirectory();
@@ -571,6 +576,7 @@ public class GrailsToolboxFrame extends javax.swing.JFrame {
 			dispose();
 		}
 	}
+
 	private void setGrailsProjectDirectory() {
 		try {
 			File newDirectory = ComponentUtils.selectReadDirectoryViaFileChooser(this);
@@ -584,7 +590,7 @@ public class GrailsToolboxFrame extends javax.swing.JFrame {
 			handleException(e);
 		}
 	}
-	
+
 	private File fetchGrailsDirectory() throws IOException {
 		File directory = IoUtils.fetchPreviousProject();
 
@@ -594,7 +600,7 @@ public class GrailsToolboxFrame extends javax.swing.JFrame {
 		}
 		return directory;
 	}
-	
+
 	private boolean grailsIsInstalled() throws Exception {
 		try {
 			executeGrailsProcess(new VersionGrailsProcess());
@@ -604,18 +610,19 @@ public class GrailsToolboxFrame extends javax.swing.JFrame {
 			return false;
 		}
 	}
-	
+
 	private void handleException(Exception exception) {
 		exception.printStackTrace(System.err);
 		ExceptionDialog.showDialog(this, exception);
 		try {
 			currentProcess.stop();
-		} catch (IOException ex) { }
+		} catch (IOException ex) {
+		}
 	}
-	
+
     private void runAppButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_runAppButtonActionPerformed
-        String environment = runAppEnviroComboBox.getSelectedItem().toString();
-		
+		String environment = runAppEnviroComboBox.getSelectedItem().toString();
+
 		RunAppGrailsProcess runApp;
 		if (useConsoleCheckbox.isSelected()) {
 			runApp = new ConsoleGrailsProcess();
@@ -628,29 +635,29 @@ public class GrailsToolboxFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_runAppButtonActionPerformed
 
     private void testAppButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_testAppButtonActionPerformed
-        boolean withUnitTest = testAppUnitCheckBox.isSelected();
+		boolean withUnitTest = testAppUnitCheckBox.isSelected();
 		boolean withIntegTest = testAppIntegCheckBox.isSelected();
 		String classPattern = testAppPatternTextField.getText();
-		
+
 		TestAppGrailsProcess testApp = new TestAppGrailsProcess();
 		testApp.setIncludeUnitTest(withUnitTest);
 		testApp.setIncludeIntegTest(withIntegTest);
 		testApp.setClassNamePattern(classPattern);
-		
+
 		executeGrailsProcessAsBackground(testApp);
     }//GEN-LAST:event_testAppButtonActionPerformed
 
     private void cleanButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cleanButtonActionPerformed
-        CleanGrailsProcess clean = new CleanGrailsProcess();
+		CleanGrailsProcess clean = new CleanGrailsProcess();
 		clean.setCleanAllCommand(false);
-		
+
 		executeGrailsProcessAsBackground(clean);
     }//GEN-LAST:event_cleanButtonActionPerformed
 
     private void cleanAllButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cleanAllButtonActionPerformed
-        CleanGrailsProcess cleanAll = new CleanGrailsProcess();
+		CleanGrailsProcess cleanAll = new CleanGrailsProcess();
 		cleanAll.setCleanAllCommand(true);
-		
+
 		executeGrailsProcessAsBackground(cleanAll);
     }//GEN-LAST:event_cleanAllButtonActionPerformed
 
@@ -676,7 +683,7 @@ public class GrailsToolboxFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_killProcessButtonActionPerformed
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
-        setGrailsProjectDirectory();
+		setGrailsProjectDirectory();
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
@@ -691,24 +698,24 @@ public class GrailsToolboxFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void compileButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_compileButtonActionPerformed
-        CompileGrailsProcess compile = new CompileGrailsProcess();
+		CompileGrailsProcess compile = new CompileGrailsProcess();
 		compile.setGrailsProjectDirectrory(grailsProjectDirectory);
-		
+
 		executeGrailsProcessAsBackground(compile);
     }//GEN-LAST:event_compileButtonActionPerformed
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
-        AboutDialog aboutDialog = new AboutDialog(this, true);
+		AboutDialog aboutDialog = new AboutDialog(this, true);
 		aboutDialog.setLocationRelativeTo(this);
 		aboutDialog.setVisible(true);
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     private void formWindowActivated(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowActivated
-        ComponentUtils.refreshComponents(this);
+		ComponentUtils.refreshComponents(this);
     }//GEN-LAST:event_formWindowActivated
 
     private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
-        consoleTextArea.setText(null);
+		consoleTextArea.setText(null);
     }//GEN-LAST:event_jMenuItem5ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
@@ -717,8 +724,8 @@ public class GrailsToolboxFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void customCommandMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_customCommandMenuItemActionPerformed
-        String command = CustomGrailsCommandDialog.fetchCommand(this);
-		
+		String command = CustomGrailsCommandDialog.fetchCommand(this);
+
 		if (null != command) {
 			CustomGrailsProcess customCommand = new CustomGrailsProcess();
 			customCommand.setCommand(command);
@@ -727,17 +734,22 @@ public class GrailsToolboxFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_customCommandMenuItemActionPerformed
 
     private void testAppPatternTextFieldKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_testAppPatternTextFieldKeyPressed
-        if (KeyEvent.VK_ENTER == evt.getKeyCode()) {
+		if (KeyEvent.VK_ENTER == evt.getKeyCode()) {
 			testAppButtonActionPerformed(null);
 		}
     }//GEN-LAST:event_testAppPatternTextFieldKeyPressed
 
     private void toggleToolboxButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_toggleToolboxButtonActionPerformed
-        boolean toggledToolboxVisiblity = !toolboxPanel.isVisible();
-		
+		boolean toggledToolboxVisiblity = !toolboxPanel.isVisible();
+
 		toolboxPanel.setVisible(toggledToolboxVisiblity);
 		toggleToolboxButton.setText(toggledToolboxVisiblity ? "\u00ab" : "\u00bb");
     }//GEN-LAST:event_toggleToolboxButtonActionPerformed
+
+    private void formKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyTyped
+		System.out.println(evt.getKeyCode());
+		System.out.println(evt.getKeyCode());
+    }//GEN-LAST:event_formKeyTyped
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton cleanAllButton;
