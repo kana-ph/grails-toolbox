1e93e140aadbb461499f6d90044d8273e1776907
kana0011 <kana.incognito@gmail.com>
2016-02-09 00:18:19 +0800

    Implement verbose mode argument
    Signed-off-by: kana0011 <kana.incognito@gmail.com>

--- src/ph/kana/grtb/process/GrailsProcess.java
+++ src/ph/kana/grtb/process/GrailsProcess.java
@@ -6,32 +6,37 @@ import java.io.InputStream;
 import ph.kana.grtb.utils.IoUtils;
 
 public abstract class GrailsProcess {
-	
+
 	private Process osProcess;
 	private File grailsProjectDirectrory;
 	private boolean stacktraceMode;
 	private boolean verboseMode;
-	
+
 	protected abstract String[] getArgs();
-	
+
 	public final String getCommandString() {
 		StringBuilder commandString = new StringBuilder("grails");
 		for (String arg : this.getArgs()) {
 			commandString.append(" ").append(arg);
 		}
-		
+
 		if (stacktraceMode) {
 			commandString.append(" --stacktrace");
 		}
+
+		if (verboseMode) {
+			commandString.append(" --verbose");
+		}
+
 		return commandString.toString();
 	}
-	
+
 	public final void execute() throws IOException, InterruptedException {
 		String command = getCommandString();
 		IoUtils.logRunning(this);
 		osProcess = Runtime.getRuntime().exec(command, null, getGrailsProjectDirectrory());
 	}
-	
+
 	public void stop() throws IOException {
 		IoUtils.logKill(this);
 		osProcess.destroy();
@@ -48,18 +53,19 @@ public abstract class GrailsProcess {
 			throw new IllegalArgumentException("Not a directory: " + grailsProjectDirectrory.getAbsolutePath());
 		}
 	}
-	
+
 	public boolean getStacktraceMode() {
 		return stacktraceMode;
 	}
-	
+
 	public void setVerboseMode(boolean verboseMode) {
 		this.verboseMode = verboseMode;
 	}
+
 	public void setStacktraceMode(boolean useStackTraceMode) {
 		this.stacktraceMode = useStackTraceMode;
 	}
-	
+
 	public InputStream getInputStream() {
 		return osProcess.getInputStream();
 	}
