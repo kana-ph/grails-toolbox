adab3b321416fafc21022ed6bb93e1b1f5f223eb
kana0011 <kana.incognito@gmail.com>
2016-05-01 22:52:52 +0800

    Implement hiding of controls on resize +animation
    Signed-off-by: kana0011 <kana.incognito@gmail.com>

--- src/ph/kana/grtb/controller/ToolboxController.java
+++ src/ph/kana/grtb/controller/ToolboxController.java
@@ -1,4 +1,41 @@
 package ph.kana.grtb.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class ToolboxController {
+	@FXML
+	private Button  collapseButton;
+	@FXML
+	private AnchorPane rootAnchorPane;
+	@FXML
+	private AnchorPane consoleAnchorPane;
+
+	@FXML
+	public void toggleToolbox() {
+		double DEFAULT_CONSOLE_LEFT_ANCHOR = 240.0;
+		boolean expanding = DEFAULT_CONSOLE_LEFT_ANCHOR == rootAnchorPane.getLeftAnchor(consoleAnchorPane);
+
+		collapseButton.setText(expanding? "\u00bb" : "\u00ab");
+
+		Timer animationTimer = new Timer();
+		animationTimer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				double increment = expanding? -3.0 : 3.0;
+				double finalAnchor = expanding? 3.0 : DEFAULT_CONSOLE_LEFT_ANCHOR;
+
+				double currentAnchor = rootAnchorPane.getLeftAnchor(consoleAnchorPane);
+				if ((expanding && (currentAnchor > finalAnchor)) || (!expanding && currentAnchor < finalAnchor)) {
+					rootAnchorPane.setLeftAnchor(consoleAnchorPane, currentAnchor + increment);
+				} else {
+					this.cancel();
+				}
+			}
+		}, 0, 5);
+	}
 }
